32cc8d4d4014c4e62272d7fd0f63b3a3b001ae4e
RomainValmo <142975520+RomainValmo@users.noreply.github.com>
2024-11-30 14:44:46 +0100

    feat(poderror): add step 2 (#13)

--- kubernetes-pod-error/step1/verify.sh
+++ kubernetes-pod-error/step1/verify.sh
@@ -13,7 +13,7 @@ fi
 
 # Verify that the pod is in the running state
 expected="nginx   Running"
-actual=$(kubectl get pod -n exercice1 -o custom-columns="NAME:.metadata.name,STATUS:.status.phase" --no-headers)
+actual=$(kubectl get pod -n exercise1 -o custom-columns="NAME:.metadata.name,STATUS:.status.phase" --no-headers)
 
 
 if [ "$expected" == "$actual" ]; then
